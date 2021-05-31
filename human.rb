55609e93af0296e2343cc4209eea59cff82f0635
miyagawa.mizuki <wakimizu3333@gmail.com>
2021-05-31 13:23:02 +0000

    add_kadai_lesson9

---
+++ human.rb
@@ -0,0 +1,17 @@
+require "./animal" 
+require "./thinkable"
+
+class Human < Animal
+  
+  include Thinkable
+  
+  attr_accessor :hoby
+  
+  def initialize(name, age, hoby)
+    self.name = name
+    self.age = age
+    self.hoby = hoby
+  end
+  
+end
+
---
+++ thinkable.rb
@@ -0,0 +1,8 @@
+
+module Thinkable
+  
+  def think
+    puts "私は#{hoby}について考えています。"
+  end
+  
+end
